bb7a17ce969d656c866c1ba48cf9eff814e232ba
Hack Reactor Students <ilyons@gmail.com>
2014-02-26 15:16:17 -0800

    Finished queue and stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,17 +3,30 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var topQueue = 0;
+  var bottomQueue = 0;
 
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[topQueue++] = value;
   };
 
   instance.dequeue = function(){
+    if (topQueue > bottomQueue) {
+      var item = storage[bottomQueue];
+      delete storage[bottomQueue++];
+      return item;
+    }
   };
 
   instance.size = function(){
+    return topQueue - bottomQueue;
   };
 
   return instance;
 };
+
+
+
+
