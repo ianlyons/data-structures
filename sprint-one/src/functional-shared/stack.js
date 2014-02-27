58eb9487fdb27d0e489a7ca8a76a4bd4e155ca34
Hack Reactor Students <ilyons@gmail.com>
2014-02-26 16:09:53 -0800

    Finished the functional stack problem

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,28 @@
-var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
-};
-  
 var stackMethods = {};
+
+var makeStack = function() {
+  var instance = {};  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.objSize = 0; // Hint: set an initial value here
+
+  instance = _.extend(instance, stackMethods);
+
+  return instance;
+};
+
+stackMethods.push = function(value) {
+  this.storage[this.objSize++] = value;
+};
+
+stackMethods.pop = function() {
+  var item = this.storage[this.objSize-1];
+  if (this.objSize > 0) {
+    delete this.storage[this.objSize--];
+  }
+  return item;
+};
+
+stackMethods.size = function () {
+  return this.objSize;
+};
+
