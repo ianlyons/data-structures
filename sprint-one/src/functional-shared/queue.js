404411bd22f1cc4a45f68b44dd393354ac4bbe04
Hack Reactor Students <ilyons@gmail.com>
2014-02-26 16:18:19 -0800

    Through functional-shared pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,32 @@
-var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
-};
+/* global _, makeQueue */
 
 var queueMethods = {};
+
+var makeQueue = function(){
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.topQueue = 0;
+  instance.bottomQueue = 0;
+
+  _.extend(instance, queueMethods);
+
+  return instance;
+};
+
+queueMethods.enqueue = function(value){
+    this.storage[this.topQueue++] = value;
+};
+
+queueMethods.dequeue = function(){
+  if (this.topQueue > this.bottomQueue) {
+    var item = this.storage[this.bottomQueue];
+    delete this.storage[this.bottomQueue++];
+    return item;
+  }
+};
+
+queueMethods.size = function(){
+  return this.topQueue - this.bottomQueue;
+};
