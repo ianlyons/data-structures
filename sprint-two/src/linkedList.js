fc95961bf75effa4a09a19a9369b292d995ea146
Hack Reactor Students <ilyons@gmail.com>
2014-02-27 20:33:44 -0800

    LinkedList done

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,30 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var node = makeNode(value);
+    if(list.tail === null) {
+      list.head = node;
+    } else {
+      list.tail.next = node;
+    }
+    list.tail = node;
   };
 
   list.removeHead = function(){
+    var node = list.head;
+    list.head = node.next;
+    return node;
   };
 
-  list.contains = function(target, node){
+  list.contains = function(target){
+    var node = list.head;
+    while(node) {
+      if( node.value === target ) {
+        return true;
+      }
+      node = node.next;
+    }
+    return false;
   };
 
   return list;
