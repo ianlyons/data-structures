86be0ba55c1379f2df8bc7cac7c011bef7bab003
Hack Reactor Students <ilyons@gmail.com>
2014-02-27 20:15:20 -0800

    Through tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,16 +1,35 @@
 var makeTree = function(value){
+
+  var extend = function (obj, obj2) {
+    for (var key in obj2) {
+      obj[key] = obj2[key];
+    }
+  };
+
   var newTree = {};
+  extend(newTree, treeMethods);
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
   return newTree;
 };
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var node = makeTree(value);
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target){
-};
+  if(this.value === target) {
+    return true;
+  }
 
+  for(var i = 0; i < this.children.length; i++) {
+    if( this.children[i].contains(target) ) {
+      return true;
+    }
+  }
+
+  return false;
+};
