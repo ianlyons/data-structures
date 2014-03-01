127a03c559dc3f3af9cfc04c01a2b39090dd1b9b
Hack Reactor Students <ilyons@gmail.com>
2014-02-28 16:21:08 -0800

    Through the BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,58 @@
 var makeBinarySearchTree = function(value){
+  var extend = function (obj, obj2) {
+    for (var key in obj2) {
+      obj[key] = obj2[key];
+    }
+  };
+
+  var newTree = {};
+  extend(newTree, treeMethods);
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+  return newTree;
+};
+
+var treeMethods = {};
+
+treeMethods.insert = function(value) {
+  var newNode = makeBinarySearchTree(value);
+
+  if ( value > this.value ) {
+    if( this.right === null ) {
+      this.right = newNode;
+    } else {
+      this.right.insert(value);
+    }
+  } else if( value < this.value ) {
+    if( this.left === null ) {
+      this.left = newNode;
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    // throw new Error("Whoa, whoa, whoa.");
+  }
+};
+
+treeMethods.contains = function(target) {
+  // check yourself first
+  if( this.value === target ) {
+    return true;
+  } else {
+    if(target > this.value) {
+      if( (this.right) && (this.right.contains(target)) ) {
+        return true;
+      }
+    } else if (target < this.value) {
+      if( (this.left) && (this.left.contains(target)) ){
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+treeMethods.depthFirstLog = function() {
+
 };
